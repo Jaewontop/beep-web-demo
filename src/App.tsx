cd80fd27244386e5c6c3b950fcd32f937c8147cd
황재원 <91544407+Jaewontop@users.noreply.github.com>
2024-05-04 03:15:38 +0000

    "Feat: 자리비움, 핸드폰, 펜, 공부 판별하여 띄워주는 기능 추가"

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState} from "react";
 import Webcam from "react-webcam";
 import { css } from "@emotion/css";
 import { Camera } from "@mediapipe/camera_utils";
@@ -9,6 +9,8 @@ const App = () => {
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const resultsRef = useRef<Results>();
+  const [landmarkResults, setLandmarkResults] = useState('');
+  const [noHandFrame, setNoHandFrame] = useState(0);
 
   /**
    * 검출결과（프레임마다 호출됨）
@@ -19,7 +21,29 @@ const App = () => {
 
     const canvasCtx = canvasRef.current!.getContext("2d")!;
     drawCanvas(canvasCtx, results);
+    // if(results !== undefined && results !== null){
+    const responseObject = results.multiHandLandmarks;
+
+    if(responseObject.length>0){
+      const obj = responseObject[0];
+      setNoHandFrame(0);
+      if(getDistanceBetweenTwoPoints(obj[8],obj[20]) < 0.19 && (getDistanceBetweenTwoPoints(obj[4],obj[12]) < 0.065 || getDistanceBetweenTwoPoints(obj[4],obj[8]) < 0.065|| getDistanceBetweenTwoPoints(obj[4],obj[16]) < 0.065) && obj[0].y <0.49){
+        setLandmarkResults('❌ using phone')
+      }else if(obj[0].y >=0.49 && getDistanceBetweenTwoPoints(obj[4],obj[12]) < 0.13 && getDistanceBetweenTwoPoints(obj[4],obj[8]) < 0.1 && getDistanceBetweenTwoPoints(obj[4],obj[20]) < 0.20){
+        setLandmarkResults('✅ pen');
+      }
+      else if(obj[0].y >= 0.49  ){
+        setLandmarkResults('✅ studying')
+      }
+      // setLandmarkResults(JSON.stringify(responseObject[0][0]).toString());
+    }else{
+      setNoHandFrame((prev) => prev + 1);
+    }
+      
+    // }
   }, []);
+
+  
 
   // 초기설정
   useEffect(() => {
@@ -46,8 +70,8 @@ const App = () => {
         onFrame: async () => {
           await hands.send({ image: webcamRef.current!.video! });
         },
-        width: 1280,
-        height: 720,
+        width: 500,
+        height: 500,
       });
       camera.start();
     }
@@ -56,8 +80,16 @@ const App = () => {
   /*  랜드마크들의 좌표를 콘솔에 출력 */
   const OutputData = () => {
     const results = resultsRef.current!;
-    console.log(results.multiHandLandmarks);
+    const responseObject = results.multiHandLandmarks[0][0];
+    console.log(JSON.stringify(responseObject));
   };
+
+  const getDistanceBetweenTwoPoints = (point1:any, point2:any) => {
+    const x_diff = Math.abs(point1.x - point2.x)
+    const y_diff = Math.abs(point1.y - point2.y)
+    const z_diff = Math.abs(point1.z - point2.z)
+    return Math.sqrt(x_diff*x_diff + y_diff*y_diff + z_diff*z_diff)
+  }
 
   return (
     <div className={styles.container}>
@@ -65,25 +97,28 @@ const App = () => {
       <Webcam
         audio={false}
         style={{ visibility: "hidden" }}
-        width={1280}
-        height={720}
+        width={500}
+        height={500}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
-        videoConstraints={{ width: 1280, height: 720, facingMode: "user" }}
+        videoConstraints={{ width: 500, height: 500, facingMode: "user" }}
       />
       {/* 랜드마크를 손에 표시 */}
       <canvas
         ref={canvasRef}
         className={styles.canvas}
-        width={1280}
-        height={720}
+        width={500}
+        height={500}
       />
       {/* 좌표 출력 */}
-      <div className={styles.buttonContainer}>
+      {/* <div className={styles.buttonContainer}>
         <button className={styles.button} onClick={OutputData}>
           Output Data
         </button>
-      </div>
+      </div> */}
+      <p className={styles.pContainer}>{noHandFrame>=20 ? '❌ not here':landmarkResults}</p>
+      {/* <p className={styles.frameCountContainer}>{noHandFrame}</p> */}
+
     </div>
   );
 };
@@ -103,14 +138,24 @@ const styles = {
   `,
   canvas: css`
     position: absolute;
-    width: 1280px;
-    height: 720px;
+    width: 500px;
+    height: 500px;
     background-color: #fff;
   `,
   buttonContainer: css`
     position: absolute;
     top: 20px;
     left: 20px;
+  `,
+  pContainer: css`
+    position: absolute;
+    top: 40px;
+    left: 10px;
+  `,
+  frameCountContainer: css`
+    position: absolute;
+    top: 50px;
+    left: 10px;
   `,
   button: css`
     color: #fff;
